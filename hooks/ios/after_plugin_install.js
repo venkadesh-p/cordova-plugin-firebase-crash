e2c56bb8b4a02785950c34f4ba5043c4f6e4e398
Venkadesh <venkadesh.p@unvired.io>
2019-11-11 21:19:11 +0530

    Added Code to copy and link GoogleService-Info.plist to xcode project.

--- hooks/ios/after_plugin_install.js
+++ hooks/ios/after_plugin_install.js
@@ -1,11 +1,30 @@
 const fs = require("fs");
 const xcode = require("xcode");
 const helper = require("./helper");
+const path = require("path");
 
 module.exports = function(context) {
     const comment = helper.BUILD_PHASE_COMMENT;
     const xcodeProjectPath = helper.getXcodeProjectPath(context);
     const xcodeProject = xcode.project(xcodeProjectPath);
+
+    const ConfigParser = context.requireCordovaModule("cordova-lib").configparser;
+    const appName = new ConfigParser("config.xml").name();
+
+    var proj = new xcode.project(xcodeProjectPath);
+    proj.parse(function(err) {
+        if (err) {
+            console.log("Oh noes! XCODE project failed to parse:");
+            console.log(err);
+        } else {
+            var src = path.resolve('GoogleService-Info.plist')
+            var dest = path.resolve("platforms", "ios", appName, "GoogleService-Info.plist")
+            fs.copyFileSync(src, dest, (res) => {
+            })
+            proj.addResourceFile(dest);
+            fs.writeFileSync(xcodeProjectPath, proj.writeSync());
+        }
+    });
 
     xcodeProject.parseSync();
 
